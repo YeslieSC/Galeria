f3aaa44db6d340ad7426baca484bf37e10ed2db5
YeslieSC <al23760341@ite.edu.mx>
2025-02-14 18:40:56 -0800

    Agregar archivos de proyecto.

---
+++ Galeria/Form1.Designer.cs
@@ -0,0 +1,55 @@
+﻿namespace ImageGallery
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btnAddImages;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnAddImages = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            
+            // flowLayoutPanel1
+            
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(776, 370);
+            this.flowLayoutPanel1.TabIndex = 0;
+            
+            // btnAddImages
+            
+            this.btnAddImages.Location = new System.Drawing.Point(338, 400);
+            this.btnAddImages.Name = "btnAddImages";
+            this.btnAddImages.Size = new System.Drawing.Size(124, 29);
+            this.btnAddImages.TabIndex = 1;
+            this.btnAddImages.Text = "Agregar Imágenes";
+            this.btnAddImages.UseVisualStyleBackColor = true;
+            this.btnAddImages.Click += new System.EventHandler(this.btnAddImages_Click);
+            
+            // Form1
+            
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAddImages);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Image Gallery";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+    }
+}
---
+++ Galeria/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ImageGallery
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Imágenes|*.png;*.jpg;*.jpeg;*.bmp",
+                Multiselect = true
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string file in openFileDialog.FileNames)
+                {
+                    PictureBox pictureBox = new PictureBox
+                    {
+                        Image = Image.FromFile(file),
+                        Width = 100,
+                        Height = 100,
+                        SizeMode = PictureBoxSizeMode.Zoom,
+                        Tag = file // Guardar la ruta de la imagen en la propiedad Tag
+                    };
+                    pictureBox.Click += PictureBox_Click; // Añadir evento de clic
+                    flowLayoutPanel1.Controls.Add(pictureBox);
+                }
+            }
+        }
+        private void btnAddImages_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Imágenes|*.png;*.jpg;*.jpeg;*.bmp",
+                Multiselect = true
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string file in openFileDialog.FileNames)
+                {
+                    PictureBox pictureBox = new PictureBox
+                    {
+                        Image = Image.FromFile(file),
+                        Width = 100,
+                        Height = 100,
+                        SizeMode = PictureBoxSizeMode.Zoom,
+                        Tag = file // Guardar la ruta de la imagen en la propiedad Tag
+                    };
+                    pictureBox.Click += PictureBox_Click; // Añadir evento de clic
+                    flowLayoutPanel1.Controls.Add(pictureBox);
+                }
+            }
+        }
+        private void PictureBox_Click(object sender, EventArgs e)
+        {
+            PictureBox pictureBox = sender as PictureBox;
+            string filePath = pictureBox.Tag as string;
+
+            Form imageForm = new Form
+            {
+                Text = Path.GetFileName(filePath),
+                Width = 800,
+                Height = 600
+            };
+
+            PictureBox largePictureBox = new PictureBox
+            {
+                Image = Image.FromFile(filePath),
+                Dock = DockStyle.Fill,
+                SizeMode = PictureBoxSizeMode.Zoom
+            };
+
+            imageForm.Controls.Add(largePictureBox);
+            imageForm.ShowDialog();
+        }
+    }
+}
